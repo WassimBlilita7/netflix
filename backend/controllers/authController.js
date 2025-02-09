6287c660d53391a0fdb0d546259dc8a36e601c67
wassimblilita7 <wassim.blilita19@gmail.com>
2025-02-09 14:21:15 +0100

    4- Logout Logic in the backend finished

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,5 +53,10 @@ export async function login(req, res) {
 };
 
 export async function logout(req, res) {
-    res.send('Logout route');
-};
+    try {
+        res.clearCookie('jwt-netflix');
+        res.status(200).json({success:true, message: 'Logged out successfully'});
+    } catch (error) {
+        return res.status(500).json({success:false, message: 'Internal server error'});
+    }
+};  
