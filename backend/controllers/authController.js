30544ee6aaf49df0bf3ff8e8d36dc324fc1f6650
wassimblilita7 <wassim.blilita19@gmail.com>
2025-02-09 14:29:54 +0100

    5- Login Logic in the backend & completed authentification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -49,7 +49,31 @@ export async function signup(req, res) {
 };
 
 export async function login(req, res) {
-    res.send('Login route');
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({success:false, message: 'Please provide an email and password'});
+        }
+        const user = await User.findOne({email:email});
+        if(!user){
+            return res.status(404).json({success:false, message: 'Invalid credentials'});
+        }
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(404).json({success:false, message: 'Invalid credentials'});
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+        res.status(200).json({
+            success:true,
+            user : {
+                ...user._doc,
+                password: undefined
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({success:false, message: 'Internal server error'});
+    }
 };
 
 export async function logout(req, res) {
